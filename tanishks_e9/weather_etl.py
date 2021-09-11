fc190583bcf883c33f61f977eec4d376009f9bc4
Tanishk Sharma <70075553+TheAp0cryphal@users.noreply.github.com>
2021-09-10 20:10:50 -0700

    e9

---
+++ tanishks_e9/weather_etl.py
@@ -0,0 +1,41 @@
+import sys
+from pyspark.sql import SparkSession, functions, types
+
+spark = SparkSession.builder.appName('weather ETL').getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+
+assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
+assert spark.version >= '2.4' # make sure we have Spark 2.4+
+
+observation_schema = types.StructType([
+    types.StructField('station', types.StringType()),
+    types.StructField('date', types.StringType()),
+    types.StructField('observation', types.StringType()),
+    types.StructField('value', types.IntegerType()),
+    types.StructField('mflag', types.StringType()),
+    types.StructField('qflag', types.StringType()),
+    types.StructField('sflag', types.StringType()),
+    types.StructField('obstime', types.StringType()),
+])
+
+
+def main(in_directory, out_directory):
+
+    weather = spark.read.csv(in_directory, schema=observation_schema)
+
+    cleaned_data = weather.filter(
+    	(weather['qflag'].isNull()) & (weather['observation'] == 'TMAX') &
+    	(weather['station'].startswith('CA'))
+    	)
+    	
+    cleaned_data = cleaned_data.select(
+    	cleaned_data['station'], cleaned_data['date'], (cleaned_data['value'] / 10).alias('tmax') #dividing by 10 for C
+    	)
+    	
+    cleaned_data.write.json(out_directory, compression='gzip', mode='overwrite')
+
+
+if __name__=='__main__':
+    in_directory = sys.argv[1]
+    out_directory = sys.argv[2]
+    main(in_directory, out_directory)
